e68bd48f0291aa35ca525549382817079f8ed0c4
SriBYJU <95580233+SriBYJU@users.noreply.github.com>
2026-09-09 12:06:10 -0400

    feat: load resilience workbench visual layer

--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,5 @@ import './dark-overrides.css';
 import './readability.css';
 import './flagship.css';
 import './a11y-hotfix.css';
+import './resilience.css';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
